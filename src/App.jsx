481870bb801821f200ccee9524f671c5a7157f6b
DiegoDev02 <135850389+DiegoDev02@users.noreply.github.com>
2023-10-25 01:41:32 -0600

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from "react";
+import "./App.css";
+
+import { TopBar } from "./components/TopBar";
+import { PokeList } from "./components/PokeList";
+import { BottomBar } from "./components/BottomBar";
+
+// Creamos nuestra función "App"
+function App() {
+  // Devolvemos el contenido JSX que será renderizado en la página
+  return (
+    <div>
+      {/* Renderizamos el componente TopBar */}
+      <TopBar />
+      <PokeList />
+      <BottomBar />
+    </div>
+  );
+}
+
+// Exportamos el componente "App" para que pueda ser utilizado en otros archivos
+export default App;
