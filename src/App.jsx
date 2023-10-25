ea64fd9aa999d6b87dffc474d6c7f1dc9de5ebcb
DiegoDev02 <esmarfoxx14@gmail.com>
2023-10-25 02:36:50 -0600

    coments

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,8 @@ import { useState } from "react";
 import "./App.css";
 
 import { TopBar } from "./components/TopBar";
-import { PokeList } from "./components/PokeList";
-import { BottomBar } from "./components/BottomBar";
+//import { PokeList } from "./components/PokeList";
+//import { BottomBar } from "./components/BottomBar";
 
 // Creamos nuestra función "App"
 function App() {
@@ -12,8 +12,8 @@ function App() {
     <div>
       {/* Renderizamos el componente TopBar */}
       <TopBar />
-      <PokeList />
-      <BottomBar />
+      {/*<PokeList />*/}
+      {/*<BottomBar />*/}
     </div>
   );
 }
